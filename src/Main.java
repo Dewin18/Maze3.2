9d12b4624b411d85771e3830164e07d3e2bc476f
Dewin18 <dewin18@live.de>
2018-11-02 15:02:01 +0100

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,20 @@ public class Main {
 	// read a maze from a .txt file and mapps it into a 2D array of spots
 	MazeReader mr = new MazeReader(10, 20, "src/mazes/maze01.txt");
 	Spot[][] maze = mr.getMaze();
-	
+
 	// print the maze on the console
 	mr.printMaze();
 
 	System.out.println();
 
-	// declare some test indices e.g. maze[4][4];
-	int x = 4;
-	int y = 4;
+	// declare some test indices for the 2D array
+	int row = 4;
+	int column = 4;
 
-	System.out.println("Spot: " + maze[x][y]);
-	System.out.println("Spot neighbours: " + maze[x][y].getNeighbours());
+	// retrieve the spot from the location
+	Spot spot = maze[row][column];
+
+	System.out.println("Spot: " + spot);
+	System.out.println("Spot neighbours: " + spot.getNeighbours());
     }
 }
